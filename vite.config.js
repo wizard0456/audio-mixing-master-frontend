5bc8ff9e8f9c84cc335dc0f82b91109ddb7dac4f
pookie0717 <pookiemoney717@gmail.com>
2025-06-26 04:36:03 -0400

    fixed vite config issue

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  server: {
-    host: true,
-    port: 5173, // or any other port
-  },
+  // server: {
+  //   host: true,
+  //   port: 5173, // or any other port
+  // },
   // base: 'https://check.zetdigi.com/',
   // base: 'https://zetdigitesting.online/',
   base: 'https://audiomixingmastering.com/',
