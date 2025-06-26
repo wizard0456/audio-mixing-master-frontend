2d69b93e220aa5f3a5ece80b44b861f684e46ca5
pookie0717 <pookiemoney717@gmail.com>
2025-06-26 05:20:30 -0400

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,14 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173, // or any other port
+    proxy: {
+      // Proxy API requests
+      '/api': {
+        target: '/', 
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
   // base: 'https://check.zetdigi.com/',
   // base: 'https://zetdigitesting.online/',
